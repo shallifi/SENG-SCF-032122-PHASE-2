3ef8752d600d19d45985727b16fc726a64140abd
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-04-29 11:57:25 -0400

    moved new proj into its proper folder

---
+++ 01-intro-to-react/new-project/src/PokeCard.js
@@ -0,0 +1,21 @@
+function PokeCard({img, likes, name}){
+    // const newObj = {
+    //     me: "Shelby",
+    //     dog: "Tiger Rose"
+    // }
+
+    // const {dog, me} = newObj
+    // console.log(dog, me)
+
+    return (
+        <div className="poke-card">
+        <img src={img} alt={name}></img>
+        <h3>{name}</h3>
+        <h3 className="likes-num">Likes: {likes}</h3>
+        <button className="like-btn">❤️</button>
+        <button className="delete-btn submit-btn">Delete</button>
+        </div>
+    )
+  }
+
+  export default PokeCard
